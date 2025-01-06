7db4f72fee83ec2f4af0c91aea4ba3e9101d9fbc
safonovlg10 <safonovlg10@gmail.com>
2025-01-07 08:58:50 +0900

    feat: add 1 tasks

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -20,12 +20,13 @@ function getMatrixElementsSum( matrix ) {
   let sum = 0;
 
   for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix.length; j++) {
-     if(i === 0 || matrix[i - 1][j] !== 0){
-      sum += matrix[i][j];
-     }
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (i === 0 || matrix[i - 1][j] !== 0) {
+        sum += matrix[i][j];
+      }
     }
   }
+
   return sum;
 }
 
